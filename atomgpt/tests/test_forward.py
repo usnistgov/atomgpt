8d402ae9d5728292ae81f158be69c9d55ada5b58
knc6 <writetokamal.1989@gmail.com>
2025-05-25 09:40:09 -0600

    coverage run --omit="*scripts/*,*cache/*,*__pycache__/*" -m pytest -s -v atomgpt/tests

--- atomgpt/tests/test_forward.py
+++ atomgpt/tests/test_forward.py
@@ -1,7 +1,6 @@
 import os
 from atomgpt.forward_models.forward_models import main
 
-
 def test_forward():
     main("atomgpt/examples/forward_model/config.json")
     # cmd='python atomgpt/forward_models/forward_models.py --config_name atomgpt/examples/forward_model/config.json'
--- atomgpt/tests/test_inverse.py
+++ atomgpt/tests/test_inverse.py
@@ -1,3 +1,5 @@
+# coverage run --omit="*scripts/*,*cache/*,*__pycache__/*" -m pytest -s -v atomgpt/tests/
+#  coverage report -m -i
 from atomgpt.inverse_models.inverse_models import main
 import os
 import atomgpt
@@ -7,15 +9,37 @@ config = os.path.join(
 )
 print("config", config)
 
-"""
-def run_inverse():
-    try:
-        main(config)
-    except Exception as exp:
-        print("exp", exp)
-        pass
 
-"""
+# """
+def test_inverse():
+    if not torch.cuda.is_available():
+        pytest.skip("Skipping test: CUDA GPU is not available")
+    from atomgpt.inverse_models.loader import (
+        FastLanguageModel,
+        FastVisionModel,
+    )
+
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name="knc6/atomgpt_mistral_tc_supercon"
+    )
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name="unsloth/Qwen3-1.7B-Base-unsloth-bnb-4bit"
+    )
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name="unsloth/gemma-3-1b-pt-unsloth-bnb-4bit"
+    )
+    model, tokenizer = FastVisionModel.from_pretrained(
+        model_name="knc6/microscopy_gpt_llama3.2_vision_11b"
+    )
+    # try:
+    #    main(config)
+    # except Exception as exp:
+    #    print("exp", exp)
+    #    pass
+
+
+# test_inverse()
+# """
 
 # atomgpt/examples/inverse_model/config.json
 # run_inverse()
