3c3b87d62bb688c6448efd1174a3ad4e24fba62c
knc6 <writetokamal.1989@gmail.com>
2025-05-25 09:53:00 -0600

    Skip cuda

--- atomgpt/tests/test_inverse.py
+++ atomgpt/tests/test_inverse.py
@@ -3,6 +3,7 @@
 from atomgpt.inverse_models.inverse_models import main
 import os
 import atomgpt
+import torch
 
 config = os.path.join(
     atomgpt.__path__[0], "examples", "inverse_model", "config.json"
@@ -12,30 +13,29 @@ print("config", config)
 
 # """
 def test_inverse():
-    if not torch.cuda.is_available():
-        pytest.skip("Skipping test: CUDA GPU is not available")
-    from atomgpt.inverse_models.loader import (
-        FastLanguageModel,
-        FastVisionModel,
-    )
+    if torch.cuda.is_available():
+        from atomgpt.inverse_models.loader import (
+            FastLanguageModel,
+            FastVisionModel,
+        )
 
-    model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name="knc6/atomgpt_mistral_tc_supercon"
-    )
-    model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name="unsloth/Qwen3-1.7B-Base-unsloth-bnb-4bit"
-    )
-    model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name="unsloth/gemma-3-1b-pt-unsloth-bnb-4bit"
-    )
-    model, tokenizer = FastVisionModel.from_pretrained(
-        model_name="knc6/microscopy_gpt_llama3.2_vision_11b"
-    )
-    # try:
-    #    main(config)
-    # except Exception as exp:
-    #    print("exp", exp)
-    #    pass
+        model, tokenizer = FastLanguageModel.from_pretrained(
+            model_name="knc6/atomgpt_mistral_tc_supercon"
+        )
+        model, tokenizer = FastLanguageModel.from_pretrained(
+            model_name="unsloth/Qwen3-1.7B-Base-unsloth-bnb-4bit"
+        )
+        model, tokenizer = FastLanguageModel.from_pretrained(
+            model_name="unsloth/gemma-3-1b-pt-unsloth-bnb-4bit"
+        )
+        model, tokenizer = FastVisionModel.from_pretrained(
+            model_name="knc6/microscopy_gpt_llama3.2_vision_11b"
+        )
+        # try:
+        #    main(config)
+        # except Exception as exp:
+        #    print("exp", exp)
+        #    pass
 
 
 # test_inverse()
